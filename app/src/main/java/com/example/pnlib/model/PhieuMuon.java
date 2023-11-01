ebd909217d48e58535c0df78180e3440d79943a7
tiendung004 <dungptph36187@fpt.edu.vn>
2023-11-01 17:36:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pnlib/model/PhieuMuon.java
@@ -0,0 +1,123 @@
+package com.example.pnlib.model;
+
+import java.util.Date;
+
+public class PhieuMuon {
+    public Sach sach;
+    private int maPM;
+    private String maTT;
+    private int maTV;
+    private int maSach;
+    private Date ngay;
+    private int tienThue;
+    private int traSach;
+    private String ghiChu;
+
+    public String getGhiChu() {
+        return ghiChu;
+    }
+
+    public void setGhiChu(String ghiChu) {
+        this.ghiChu = ghiChu;
+    }
+
+    public PhieuMuon(int maPM, String maTT, int maTV, int maSach, Date ngay, int tienThue, int traSach, String ghiChu) {
+        this.maPM = maPM;
+        this.maTT = maTT;
+        this.maTV = maTV;
+        this.maSach = maSach;
+        this.ngay = ngay;
+        this.tienThue = tienThue;
+        this.traSach = traSach;
+        this.ghiChu = ghiChu;
+    }
+
+    public Sach getSach() {
+        return sach;
+    }
+
+    public void setSach(Sach sach) {
+        this.sach = sach;
+    }
+
+    public PhieuMuon(Sach sach, int maPM, String maTT, int maTV, int maSach, Date ngay, int tienThue, int traSach, String ghiChu) {
+        this.sach = sach;
+        this.maPM = maPM;
+        this.maTT = maTT;
+        this.maTV = maTV;
+        this.maSach = maSach;
+        this.ngay = ngay;
+        this.tienThue = tienThue;
+        this.traSach = traSach;
+        this.ghiChu = ghiChu;
+    }
+
+    public PhieuMuon() {
+    }
+
+    public PhieuMuon(int maPM, String maTT, int maTV, int maSach, Date ngay, int tienThue, int traSach) {
+        this.maPM = maPM;
+        this.maTT = maTT;
+        this.maTV = maTV;
+        this.maSach = maSach;
+        this.ngay = ngay;
+        this.tienThue = tienThue;
+        this.traSach = traSach;
+    }
+
+    public int getMaPM() {
+        return maPM;
+    }
+
+    public void setMaPM(int maPM) {
+        this.maPM = maPM;
+    }
+
+    public String getMaTT() {
+        return maTT;
+    }
+
+    public void setMaTT(String maTT) {
+        this.maTT = maTT;
+    }
+
+    public int getMaTV() {
+        return maTV;
+    }
+
+    public void setMaTV(int maTV) {
+        this.maTV = maTV;
+    }
+
+    public int getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(int maSach) {
+        this.maSach = maSach;
+    }
+
+    public Date getNgay() {
+        return ngay;
+    }
+
+    public void setNgay(Date ngay) {
+        this.ngay = ngay;
+    }
+
+    public int getTienThue() {
+        return tienThue;
+    }
+
+    public void setTienThue(int tienThue) {
+        this.tienThue = tienThue;
+    }
+
+    public int getTraSach() {
+        return traSach;
+    }
+
+    public void setTraSach(int traSach) {
+        this.traSach = traSach;
+    }
+}
